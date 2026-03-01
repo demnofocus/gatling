d0f04272c04d1d3fbfb4f840d53d33e7e14c3dd3
Kevin Kularathne <47452568+demnofocus@users.noreply.github.com>
2026-03-02 00:38:40 +0530

    good

--- java-maven/actions/run-gatling.sh
+++ java-maven/actions/run-gatling.sh
@@ -14,6 +14,8 @@ SIMULATION="$1"
 TEST_DURATION="$2"
 RAMP_DURATION="$3"
 WORKSPACE="$4"
+SECRET_ID="$5"
+SECRET_KEY="$6"
 
 echo "Info: Starting Gatling Tests..."
 echo "Info: Simulation: ${SIMULATION}"
@@ -33,4 +35,6 @@ echo "Info: Running Maven Gatling..."
 ./mvnw -q -B clean gatling:test \
   -Dgatling.simulationClass="${SIMULATION}" \
   -Dtest_duration="${TEST_DURATION}" \
-  -Dramp_up_duration="${RAMP_DURATION}"
+  -Dramp_up_duration="${RAMP_DURATION}" \
+  -Dsecret_id="${SECRET_ID}" \
+  -Dsecret_key="${SECRET_KEY}"
--- java-maven/src/test/java/endpoints/Pokemon.java
+++ java-maven/src/test/java/endpoints/Pokemon.java
@@ -1,23 +1,46 @@
 package endpoints;
 
+import java.util.Map;
+
+import static io.gatling.javaapi.core.CoreDsl.exec;
 import static io.gatling.javaapi.core.CoreDsl.csv;
 import static io.gatling.javaapi.core.CoreDsl.feed;
+import static io.gatling.javaapi.core.CoreDsl.ElFileBody;
 import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.header;
 import static io.gatling.javaapi.http.HttpDsl.status;
+import static io.gatling.javaapi.http.HttpDsl.addCookie;
+import static io.gatling.javaapi.http.HttpDsl.Cookie;
 
 import io.gatling.javaapi.core.ChainBuilder;
 import io.gatling.javaapi.core.FeederBuilder;
 
+
 public class Pokemon {
 
-  private static final FeederBuilder.Batchable<String> POKEMON_NAME_FDR =
-      csv("data/pokemon_names.csv").random();
+    private static final FeederBuilder.Batchable<String> POKEMON_NAME_FDR = csv("data/pokemon_names.csv").random();
 
-  public static ChainBuilder getPokemonDetails() {
-    return feed(POKEMON_NAME_FDR)
-        .exec(
-            http(RequestName.T01_GET_POKEMON_DETAILS_BY_NAME.name())
-                .get("pokemon/#{p_name}")
-                .check(status().is(200)));
-  }
+    private static final Map<CharSequence, String> AUTH_HDR = Map.of("Content-Type", "application/json");
+
+    private static final String SECRET_ID = System.getProperty("secret_id", "");
+    private static final String SECRET_KEY = System.getProperty("secret_key", "");
+
+    public static ChainBuilder authentication() {
+        return exec(session -> session.set("secret_id", SECRET_ID).set("secret_key", SECRET_KEY))
+                .exec(http(RequestName.T00_AUTHENTICATION.name())
+                        .post("pokemon/auth") //not real endpoint
+                        .body(ElFileBody("payload/auth.json"))
+                        .headers(AUTH_HDR)
+                        .check(header("X-AUTH-TOKEN").saveAs("token"))
+                ).exec(addCookie(Cookie("X_AUTH_TOKEN", "#{token}")));
+    }
+
+    public static ChainBuilder getPokemonDetails() {
+        return feed(POKEMON_NAME_FDR)
+                .exec(
+                        http(RequestName.T01_GET_POKEMON_DETAILS_BY_NAME.name())
+                                .get("pokemon/#{p_name}")
+                                .check(status().is(200)));
+    }
+
 }
